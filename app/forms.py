f68d16c522139d171bf006a1adb9d7f83fb0feb6
Evgenii Sergunin <mckrei@ya.ru>
2023-05-29 16:56:01 +0300

    first commit

---
+++ app/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField, SubmitField, EmailField, IntegerField
+from wtforms.validators import DataRequired, Length, Optional, Email
+
+
+class NewQuery(FlaskForm):
+    user = StringField(
+        'Название',
+        validators=[DataRequired(message="Поле не должно быть пустым")]
+    )
+    email = EmailField(
+        'Введите email',
+        validators=[DataRequired(message="Поле не должно быть пустым"),
+                    Email(message='Введите email')]
+    )
+    query = StringField(
+        'Запрос поиска',
+        validators=[DataRequired(message="Поле не должно быть пустым")]
+    )
+    percent = IntegerField(
+        'Процент скидки',
+        validators=[DataRequired(message="Поле не должно быть пустым")]
+    )
+
+
+class EmailSearch(FlaskForm):
+    email = EmailField(
+        'Введите email',
+        validators=[DataRequired(message="Поле не должно быть пустым"),
+                    Email(message='Введите email')]
+    )
