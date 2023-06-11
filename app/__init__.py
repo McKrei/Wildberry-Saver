b7de3bab54f29a7e3e0976c75373ce140ff7506b
Evgenii Sergunin <mckrei@ya.ru>
2023-06-11 13:07:14 +0000

    Добавил настроей деплой

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,6 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__, static_folder='static')
-# app.config.from_object('config.Config')
-
-# db = SQLAlchemy(app)
-
-# from . import models, views  # noqa
-
-# db.create_all()
-
-
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import threading
-import schedule
-import time
+
 
 app = Flask(__name__, static_folder='static')
 app.config.from_object('config.Config')
